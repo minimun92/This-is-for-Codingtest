c272d4c239214ac4d8f6cfb20eba1e1700a76f13
minimun92 <77008726+minimun92@users.noreply.github.com>
2021-12-22 03:53:04 +0900

    Create dp_Q33.py

---
+++ dp_Q33.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+T,P = [0]*n, [0]*n
+for i in range(n):
+    T[i],P[i] = map(int, input().split())
+
+
+dp = [0] * 25
+
+for i in range(n):
+    if dp[i] > dp[i+1]:
+        dp[i+1] = dp[i]
+    if dp[i+T[i]] < dp[i] + P[i]:
+        dp[i+T[i]] = dp[i] + P[i]
+
+
+print(dp[n])
