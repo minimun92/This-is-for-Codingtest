b654a209185440f74ffc7eff568c042b56daa729
minimun92 <77008726+minimun92@users.noreply.github.com>
2021-12-22 14:41:15 +0900

    Update dp_Q33.py

--- dp_Q33.py
+++ dp_Q33.py
@@ -10,8 +10,10 @@ for i in range(n):
 dp = [0] * 20
 
 for i in range(n):
-    if dp[i] > dp[i+1]:
+    # i일까지의 보상이 다음날까지의 보상보다 크면 갱신
+    if dp[i+1] < dp[i]:
         dp[i+1] = dp[i]
+    # T일 후에 받을 금액이 현재 보상보다 크면 갱신
     if dp[i+T[i]] < dp[i] + P[i]:
         dp[i+T[i]] = dp[i] + P[i]
 
