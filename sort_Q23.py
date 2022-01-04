6189a5f4c76245c223d24a3dbfaef6ef6fbf6aa5
minimun92 <77008726+minimun92@users.noreply.github.com>
2022-01-05 03:27:13 +0900

    Create sort_Q23.py

---
+++ sort_Q23.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+data = []
+for i in range(n):
+    data.append(input().split())
+
+data.sort(key=lambda x : (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for i in data:
+    print(i[0])
